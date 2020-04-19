f5a3fc50bce9f0a5252101f9f40c3451aba19e6d
landscht <tony.landschoot@outlook.com>
2020-04-19 21:28:27 +0200

    refactor (settings): add form material

--- src/app/pages/page-settings/change-email/change-email.page.ts
+++ src/app/pages/page-settings/change-email/change-email.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ModalController, ToastController} from "@ionic/angular";
 import {ChangeEmailRequest} from "../../../_models/ChangeEmailRequest";
 import {ToastService} from "../../../services/toast.service";
+import {FormControl, Validators} from "@angular/forms";
+import {ErrorMatcherService} from "../../../services/error-matcher.service";
 
 @Component({
   selector: 'app-change-email',
@@ -11,6 +13,18 @@ import {ToastService} from "../../../services/toast.service";
 export class ChangeEmailPage implements OnInit {
 
   public changeEmailRequest: ChangeEmailRequest = new ChangeEmailRequest();
+
+  public emailFormControl: FormControl = new FormControl('', [
+    Validators.required,
+  ]);
+  public passwordFormControl: FormControl = new FormControl('', [
+    Validators.required,
+  ]);
+  public newEmailFormControl: FormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern('^\\w+([\\.-]?\w+)*@\\w+([\.-]?\\w+)*(\\.\\w{2,3})+$'),
+  ]);
+  public matcher: ErrorMatcherService = new ErrorMatcherService();
 
   constructor(private modalController: ModalController, private toastService: ToastService) { }
 
--- src/app/pages/page-settings/delete-account/delete-account.module.ts
+++ src/app/pages/page-settings/delete-account/delete-account.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { DeleteAccountPageRoutingModule } from './delete-account-routing.module';
 
 import { DeleteAccountPage } from './delete-account.page';
+import {MaterialModule} from "../../../material.module";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    DeleteAccountPageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        DeleteAccountPageRoutingModule,
+        MaterialModule,
+        ReactiveFormsModule
+    ],
   declarations: [DeleteAccountPage]
 })
 export class DeleteAccountPageModule {}
